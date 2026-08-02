9a7350f0de92af3dc68f7bb8266436755f534f73
EvaSokol <ewsik.s@gmail.com>
2026-08-02 18:16:52 +0300

    started to build the widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import CountriesWidget from "./components/CountriesWidget";
-import Link from "next/link";
 
 export default function Home() {
   return (
       <main>
           <h1>Home page</h1>
-          <Link href="/about">About Us</Link>
         <CountriesWidget />
       </main>
   );
